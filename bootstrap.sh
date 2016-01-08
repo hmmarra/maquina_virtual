00c45180329699d2ef269ffe3d051dff4044ac52
matheus.cburger <matheus.cburger@gmail.com>
2016-01-08 19:20:39 -0200

    Baixa dados do software carpentry

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,10 +9,13 @@ gpg -a --export E084DAB9 | sudo apt-key add -
 
 apt-get update
 
-apt-get install -y vim r-base r-base-dev gdebi-core
+apt-get install -y vim r-base r-base-dev gdebi-core x11-apps
 
 wget http://download2.rstudio.org/rstudio-server-0.99.491-i386.deb
 
 gdebi -n rstudio-server-0.99.491-i386.deb
 
-
+wget "http://swcarpentry.github.io/shell-novice/shell-novice-data.zip"
+mkdir -p  ~vagrant/Desktop/shell-novice
+unzip -d ~vagrant/Desktop/shell-novice shell-novice-data.zip
+chown -R vagrant ~vagrant/Desktop/shell-novice
